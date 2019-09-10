c6056671d8ac45c00138366c7941ff49ba316e5d
Rosano Coutinho <pub@xeari.com>
2019-09-10 15:02:19 -0400

    Stub popover view

---
+++ open-popover/controller.js
@@ -0,0 +1,11 @@
+exports.OLSKControllerRoutes = function() {
+	return {
+		LBXPopoverRoute: {
+			OLSKRoutePath: '/popover',
+			OLSKRouteMethod: 'get',
+			OLSKRouteFunction: function (req, res, next) {
+				return res.render(require('path').join(__dirname, 'stub-view'));
+			},
+		},
+	};
+};
