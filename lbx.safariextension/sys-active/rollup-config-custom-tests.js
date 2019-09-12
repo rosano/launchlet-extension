c5388342f75dde285b844eee0ddc3b6070af3bf5
Rosano Coutinho <pub@xeari.com>
2019-09-12 10:10:59 -0400

    Spec LBXActiveRollupConfigCustom

---
+++ lbx.safariextension/sys-active/rollup-config-custom-tests.js
@@ -0,0 +1,22 @@
+import { throws, doesNotThrow, deepEqual } from 'assert';
+
+import * as mainModule from './rollup-config-custom.js';
+
+describe('LBXActiveRollupConfigCustom', function testLBXActiveRollupConfigCustom() {
+
+	it('throws error if not object', function() {
+		throws(function() {
+			mainModule.LBXActiveRollupConfigCustom(null);
+		}, /LCHErrorInputInvalid/);
+	})
+
+	it('removes livereload', function() {
+		deepEqual(mainModule.LBXActiveRollupConfigCustom({
+			output: {},
+			plugins: [{
+				name: 'livereload',
+			}],
+		}).plugins, []);
+	});
+
+});
---
+++ lbx.safariextension/sys-active/rollup-config-custom.js
@@ -0,0 +1,18 @@
+const pathPackage = require('path');
+
+module.exports = {
+	LBXActiveRollupConfigCustom (inputData, options) {
+		if (typeof inputData !== 'object' || inputData === null) {
+			throw new Error('LCHErrorInputInvalid');
+		}
+
+		inputData.plugins.splice(inputData.plugins.indexOf(inputData.plugins.filter(function (e) {
+			return e.name === 'livereload';
+		}).pop()), 1);
+
+		return inputData;
+	},
+	OLSKRollupConfigCustom (inputData, options) {
+		return module.exports.LBXActiveRollupConfigCustom(inputData, options);
+	},
+};
