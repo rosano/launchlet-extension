cd96ab9dcc920f25fe908032a4bc66f1f01aa216
Rosano Coutinho <pub@xeari.com>
2019-09-12 09:44:06 -0400

    Scaffold @MessageReceiveFromPage

--- lbx.safariextension/sys-active/main.js
+++ lbx.safariextension/sys-active/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const mod = {
+
+	// MESSAGE
+
+	MessageReceiveFromPage (event) {
+		// We only accept messages from ourselves
+		if (event.source !== window) {
+		  return;
+		}
+
+		// We only accept messages from ourselves
+		// if (not launchlet.dev) {
+		//   return;
+		// }
+		console.log('stub check event source');
+	}
+
+	// SETUP
+
+	SetupEveryting() {
+		mod.SetupReceiveMessageFromPage();
+	},
+	SetupReceiveMessageFromPage() {
+		// @MessageReceiveFromPage
+		window.addEventListener('message', mod.MessageReceiveFromPage, false);
+	},
+
+	// LIFECYCLE
+
+	LifecyclePageDidLoad() {
+		mod.SetupEveryting();
+	},
+};
+
+mod.LifecyclePageDidLoad();
