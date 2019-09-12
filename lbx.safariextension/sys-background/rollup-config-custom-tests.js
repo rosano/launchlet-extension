60ed39071feb190d61247998a4c306ed07513dd7
Rosano Coutinho <pub@xeari.com>
2019-09-12 11:03:30 -0400

    Setup background rollup

---
+++ lbx.safariextension/sys-background/rollup-config-custom-tests.js
@@ -0,0 +1,22 @@
+import { throws, deepEqual } from 'assert';
+
+import * as mainModule from './rollup-config-custom.js';
+
+describe('LBXBackgroundRollupConfigCustom', function testLBXBackgroundRollupConfigCustom() {
+
+	it('throws error if not object', function() {
+		throws(function() {
+			mainModule.LBXBackgroundRollupConfigCustom(null);
+		}, /LCHErrorInputInvalid/);
+	})
+
+	it('removes livereload', function() {
+		deepEqual(mainModule.LBXBackgroundRollupConfigCustom({
+			output: {},
+			plugins: [{
+				name: 'livereload',
+			}],
+		}).plugins, []);
+	});
+
+});
