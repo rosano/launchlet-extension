1f54fe0305ab012cf62cb22e47426cd6a7fc408c
Rosano Coutinho <pub@xeari.com>
2019-10-03 15:07:45 -0400

    Purge global file

--- lbx.safariextension/-shared/LBXGlobal/main.js
+++
@@ -1,5 +0,0 @@
-let _translationDictionary = JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`);
-import OLSKInternational from 'OLSKInternational';
-export const OLSKLocalized = function(translationConstant) {
-	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, _translationDictionary[OLSKInternational.OLSKInternationalSimplifiedLanguageCode(navigator.language)]);
-};
--- lbx.safariextension/open-popover/main.svelte
+++ lbx.safariextension/open-popover/main.svelte
@@ -1,7 +1,11 @@
 <script>
 import api from './api.js';
 
-import { OLSKLocalized } from '../-shared/LBXGlobal/main.js';
+import OLSKInternational from 'OLSKInternational';
+export const OLSKLocalized = function(translationConstant) {
+	return OLSKInternational.OLSKInternationalLocalizedString(translationConstant, JSON.parse(`{"OLSK_I18N_SEARCH_REPLACE":"OLSK_I18N_SEARCH_REPLACE"}`)[OLSKInternational.OLSKInternationalSimplifiedLanguageCode(navigator.language)]);
+};
+
 import { OLSK_TESTING_BEHAVIOUR } from 'OLSKTesting';
 
 import { LBXPopoverRandomSeed } from './ui-logic.js';
