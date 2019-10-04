857a30b26479b2e656be14f7a56e6039f7557617
Rosano Coutinho <pub@xeari.com>
2019-10-04 12:27:07 -0400

    Stub popover view for localization test

--- lbx.safariextension/open-popover/controller.js
+++ lbx.safariextension/open-popover/controller.js
@@ -4,9 +4,9 @@ exports.OLSKControllerRoutes = function() {
 			OLSKRoutePath: '/popover',
 			OLSKRouteMethod: 'get',
 			OLSKRouteFunction: function (req, res, next) {
-				return res.render(require('path').join(__dirname, 'ui-view.html'));
+				return res.render(require('path').join(__dirname, 'stub-view.ejs'));
 			},
-			OLSKRouteLanguages: ['en'],
+			OLSKRouteLanguages: ['en', 'fr', 'es'],
 		},
 	};
 };
