b73b2f08ccfa03b172f37de67052ab005033e171
Rosano Coutinho <pub@xeari.com>
2019-12-13 19:24:54 -0500

    Rename OLSKRollupScanStart

--- rollup-config.js
+++ rollup-config.js
@@ -1,6 +1,6 @@
-import { OLSKRollupScanStart } from 'OLSKRollup';
+import { OLSKRollupScaffoldScanStart } from 'OLSKRollup';
 
-export default OLSKRollupScanStart(require('path').join(__dirname, 'lbx.safariextension'), {
+export default OLSKRollupScaffoldScanStart(require('path').join(__dirname, 'lbx.safariextension'), {
 	OLSKRollupPluginSwapTokens: {
 		"'LBX_ACTIVE_MESSAGE_PAGE_ORIGIN_STRING'": JSON.stringify(process.env.LBX_ACTIVE_MESSAGE_PAGE_ORIGIN_STRING.split(','))
 	},
