d24647009cb5278365e334984de1de6d27d4a69f
Rosano Coutinho <pub@xeari.com>
2020-11-16 10:59:55 -0300

    Pass NODE_ENV to hotfix

--- package-postinstall.js
+++ package-postinstall.js
@@ -1,18 +1,14 @@
-(function ROCOHotfixSimplecryptoForUITests() {
-	if (process.env.NODE_ENV === 'production') {
-		return;
-	}
-
-	let filePath = './node_modules/simplecrypto/src/simplecrypto.js';
-	require('fs').writeFileSync(filePath, require('fs')
-		.readFileSync(filePath, 'utf8')
-		.replace(
-			'var _crypto = window.crypto || window.msCrypto;',
-			`var _crypto = window.crypto || window.msCrypto
-    if (!_crypto) {
-        return
-    };`)
-	);
+(function OLSKPostinstallHotfix() {
+	Object.entries(require('OLSKHotfix').OLSKHotfixPatches(process.env.NODE_ENV)).forEach(function ([path, patches]) {
+		if (!require('fs').existsSync(path)) {
+			return;
+		}
+		
+		Object.entries(patches).forEach(function ([search, replace]) {
+			require('fs').writeFileSync(path, require('OLSKString').OLSKStringPatch(
+				require('fs').readFileSync(path, 'utf8'), search, replace));
+		});
+	});
 })();
 
 (function OLSKPostinstallExternalAssets() {
